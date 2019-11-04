9cdb3caed078799177fe8828fb456fac5bbcec27
Quang Dat Pham <quangdat2000.pham@gmail.com>
2019-11-04 22:03:57 +0700

    refactor main.scss

--- components/MealView.jsx
+++ components/MealView.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function MealView({ meal }) {
   return (
     <div className="meal">
-      <div className="meal_image-wrapper">
+      <div className="meal__image-wrapper">
         <img className="meal__image" src={meal.imageSrc} alt={meal.name} />
       </div>
       <span className="meal__name">{meal.name}</span>
