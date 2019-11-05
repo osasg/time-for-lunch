bc0e6f86ef3a549a50b0ed72c1df466af0bc270b
Quang Dat Pham <quangdat2000.pham@gmail.com>
2019-11-05 15:59:28 +0700

    update icons, add DashboardNav, AdminIndex

---
+++ components/DashboardNav.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Link from 'next/link';
+
+import WhiteHomeIcon from '../static/icons/white-home.svg';
+import WhiteMealsIcon from '../static/icons/white-meals.svg';
+import WhiteUsersIcon from '../static/icons/white-users.svg';
+import WhiteTodayMealsIcon from '../static/icons/white-today-meals.svg';
+import WhiteAnalystIcon from '../static/icons/white-analyst.svg';
+import WhiteDeliveryIcon from '../static/icons/white-delivery.svg';
+
+import HomeIcon from '../static/icons/home.svg';
+import MealsIcon from '../static/icons/meals.svg';
+import UsersIcon from '../static/icons/users.svg';
+import TodayMealsIcon from '../static/icons/today-meals.svg';
+import AnalystIcon from '../static/icons/analyst.svg';
+import DeliveryIcon from '../static/icons/delivery.svg';
+
+const icons = {
+  WhiteHomeIcon,
+  WhiteMealsIcon,
+  WhiteUsersIcon,
+  WhiteTodayMealsIcon,
+  WhiteAnalystIcon,
+  WhiteDeliveryIcon,
+  HomeIcon,
+  MealsIcon,
+  UsersIcon,
+  TodayMealsIcon,
+  AnalystIcon,
+  DeliveryIcon
+};
+
+const Control = ({ currentBoard }) => {
+  return (
+    <div className="control">
+      {['Home', 'Meals', 'Users', 'TodayMeals', 'Analyst','Delivery'].map((v, i) => {
+        const BoardIcon = icons[(currentBoard === v ? '' : 'White') + v + 'Icon'];
+
+        return (
+          <div key={i} className={`control__tab ${currentBoard === v ? 'control__tab--current' : ''}`}>
+            <BoardIcon />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+const DashboardNav = ({ currentBoard }) => {
+  return (
+    <div className="dashboard-nav">
+      <div className="d-logo">
+        <div className="d-logo__chars">
+          <span>L</span>
+          <span>U</span>
+          <span>N</span>
+          <span>C</span>
+          <span>H</span>
+        </div>
+      </div>
+      <Control currentBoard={currentBoard} />
+    </div>
+  );
+}
+
+export default DashboardNav;
---
+++ pages/admin/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Link from 'next/link';
+
+import withDashboardLayout from '../../components/WithDashboardLayout';
+import DashboardNav from '../../components/DashboardNav';
+
+import MealsIcon from '../../static/icons/meals.svg';
+import UsersIcon from '../../static/icons/users.svg';
+import TodayMealsIcon from '../../static/icons/today-meals.svg';
+import AnalystIcon from '../../static/icons/analyst.svg';
+import DeliveryIcon from '../../static/icons/delivery.svg';
+
+const icons = {
+  MealsIcon,
+  UsersIcon,
+  TodayMealsIcon,
+  AnalystIcon,
+  DeliveryIcon
+};
+
+const Board = ({ name }) => {
+  const BoardIcon = icons[name + 'Icon'];
+
+  return (
+    <Link href={`${name.toLowerCase()}`}>
+      <a className="board" onClick={() => {}}>
+        <div className="board__icon-wrapper"><BoardIcon /></div>
+        <span className="board__name">{name}</span>
+      </a>
+    </Link>
+  );
+}
+
+const AdminIndex = () => {
+  return (
+    <div className="dashboard-index">
+      <DashboardNav currentBoard={'Home'} />
+      <Board name="Meals"/>
+      <Board name="Users"/>
+      <Board name="TodayMeals"/>
+      <Board name="Analyst"/>
+      <Board name="Delivery"/>
+    </div>
+  );
+}
+
+export default withDashboardLayout(AdminIndex);
