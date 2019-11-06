ad1979f829cbea0e1f8aadf0fe8c6ebeb5d9ff59
Quang Dat Pham <quangdat2000.pham@gmail.com>
2019-11-06 11:28:41 +0700

    add AdminHead, searchbox

---
+++ components/AdminHead.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import { observer } from 'mobx-react';
+import { observable, action } from 'mobx';
+
+import SearchIcon from '../static/icons/search.svg';
+
+@observer class AdminHead extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.searchTxt = React.createRef();
+    this.searchBtn = React.createRef();
+  }
+
+  @action handleToggleSearch = e => {
+    if (e.currentTarget.classList.contains('open-search'))
+      return this.props.handleFilterMeals(this.searchTxt.current.value);
+
+    e.currentTarget.classList.add('open-search');
+    setTimeout(() =>
+      this.searchTxt.current.classList.add('open-search')
+      || this.searchTxt.current.focus()
+    , 200);
+  }
+
+  componentDidMount = () => {
+    this.searchTxt.current.addEventListener("keyup", () => this.searchBtn.current.click());
+  }
+
+  render() {
+    const { searchStr } = this.props;
+    return (
+      <div className="admin-head">
+        <h1 className="admin-title">Meals</h1>
+        <input ref={this.searchTxt} className="search-txt" type="text" defaultValue={searchStr} />
+        <div ref={this.searchBtn} className="search-btn" onClick={this.handleToggleSearch}>
+          <SearchIcon />
+        </div>
+      </div>
+    );
+  }
+};
+
+AdminHead.propTypes = {
+  searchStr: PropTypes.string,
+  handleFilterMeals: PropTypes.func
+}
+
+export default AdminHead;
--- pages/admin/meals.jsx
+++ pages/admin/meals.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import { extendObservable, action } from 'mobx';
+import { observable, extendObservable, action } from 'mobx';
 
+import AdminHead from '../../components/AdminHead';
 import MealView from '../../components/MealView';
 import DashboardNav from '../../components/DashboardNav';
 import withDashboadLayout from '../../components/WithDashboardLayout';
@@ -11,6 +12,7 @@ import data from '../../data-sample.json';
 class AdminMealsState {
   constructor({ meals, filterMeals }) {
     extendObservable(this, {
+      searchStr: '',
       meals: data.todayMeals,
       filterMeals: data.todayMeals
     });
@@ -19,27 +21,27 @@ class AdminMealsState {
   @action handleFilterMeals = str => {
     const strToFilter = str.toLowerCase().split(/\s/).join('');
     this.filterMeals = this.meals.filter(m =>
-      m.name.toLowerCase().spilt(/\s/).join('').include(strToFilter));
+      m.name.toLowerCase().split(/\s/).join('').includes(strToFilter));
   }
 }
 
-const adminMealsState = new AdminMealsState({
+const adminMealsState = observable(new AdminMealsState({
   meals: data.todayMeals,
   filterMeals: data.todayMeals
-});
+}));
 
-const AdminMeals = ({ filterMeals, handleFilterMeals }) => {
+const AdminMeals = observer(() => {
   return (
     <div className="admin-meals">
       <DashboardNav />
-      <h1 className="admin-title">Meals</h1>
+      <AdminHead handleFilterMeals={adminMealsState.handleFilterMeals} searchStr={adminMealsState.searchStr} />
       <div className="meals-list">
-        {filterMeals.map((meal, i) =>
-          <MealView key={i} meal={meal} handleOnClick={() => appState.pickMeal(meal.id)}/>
+        {adminMealsState.filterMeals.map((meal, i) =>
+          <MealView key={i} meal={meal} handleOnClick={() => {}} />
         )}
       </div>
     </div>
   );
-}
+});
 
-export default withDashboadLayout(() => <AdminMeals {...adminMealsState}/>);
+export default withDashboadLayout(() => <AdminMeals />);
